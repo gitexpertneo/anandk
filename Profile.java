9830f79ba8586da65fadbe53fbc09808ad3636c4
gitexpertneo <gitexpert.neo@gmail.com>
2022-05-29 17:36:28 +0530

    my new program

---
+++ Profile.java
@@ -0,0 +1,9 @@
+class profile
+{
+public static void main(String args[])
+{
+System.out.println("name      : Anand");
+System.out.println("age       : 25");
+System.out.println("Document  : true");
+}
+}
